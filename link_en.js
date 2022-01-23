d02aeabfa3a579a68352171e28f28cf169da3bda
hge2020 <hge2020@korea.ac.kr>
2022-01-24 00:20:14 +0900

    암호화 분리

---
+++ link_en.js
@@ -0,0 +1,24 @@
+function change(){
+    var gett = document.getElementById("value").value;
+    if (gett == 1234){
+        location.href='https://hge2020.github.io/stay_healthy/valid_1.html'; // 테스트페이지
+    }
+    else if (gett == "MTB3AM"){
+        location.href='https://hge2020.github.io/stay_healthy/valid_2.html'; //비공개셀카
+    }
+    else if (gett == "R9JFWA"){
+        location.href='https://hge2020.github.io/stay_healthy/valid_3.html'; //비공개셀카
+    }
+    else if (gett == "ALP9AF"){
+        location.href='https://hge2020.github.io/stay_healthy/valid_4.html'; //원하는대로 데이트
+    }
+    else if (get == "CVR852"){
+        location.href='https://hge2020.github.io/stay_healthy/valid_5.html'; // 5만원 상당의 선물
+    }
+    else if (get == "44ASQM"){
+        location.href='https://hge2020.github.io/stay_healthy/valid_6.html'; // 하루 데이트비용 쏘기
+    }
+    else {
+        location.href='https://hge2020.github.io/stay_healthy/invalid.html';
+    }
+}
