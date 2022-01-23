08750c6cd35ccbc1b47225a54c7e709eb0729e8c
hge2020 <hge2020@korea.ac.kr>
2022-01-23 23:09:13 +0900

    js 뜯기

---
+++ link.js
@@ -0,0 +1,9 @@
+function change(){
+    var gett = document.getElementById("value").value;
+    if (gett == 1234){
+        location.href='https://hge2020.github.io/stay_healthy/valid_1.html';
+    }
+    else {
+        location.href='https://hge2020.github.io/stay_healthy/invalid.html';
+    }
+}
