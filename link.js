5d6db22209ffc2a91589c0091832251b9efd7fb9
hge2020 <hge2020@korea.ac.kr>
2022-01-24 00:16:05 +0900

    variation

--- link.js
+++ link.js
@@ -1,1 +1,24 @@
-eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('b c(){e 2=a.d("3").3;j(2==i){0.5=\'4://6.8.7/9/h.1\'}f{0.5=\'4://6.8.7/9/g.1\'}}',20,20,'location|html|gett|value|https|href|hge2020|io|github|stay_healthy|document|function|change|getElementById|var|else|invalid|valid_1|1234|if'.split('|'),0,{}))
+function change(){
+    var gett = document.getElementById("value").value;
+    if (gett == 1234){
+        location.href='https://hge2020.github.io/stay_healthy/valid_1.html'; // 테스트페이지
+    }
+    else if (gett == "MTB3AM"){
+        location.href='https://hge2020.github.io/stay_healthy/valid_2.html'; //비공개셀카
+    }
+    else if (gett == "R9JFWA"){
+        location.href='https://hge2020.github.io/stay_healthy/valid_3.html'; //비공개셀카
+    }
+    else if (gett == "ALP9AF"){
+        location.href='https://hge2020.github.io/stay_healthy/valid_4.html'; //원하는대로 데이트
+    }
+    else if (get == "CVR852"){
+        location.href='https://hge2020.github.io/stay_healthy/valid_5.html'; // 5만원 상당의 선물
+    }
+    else if (get == "44ASQM"){
+        location.href='https://hge2020.github.io/stay_healthy/valid_6.html'; // 하루 데이트비용 쏘기
+    }
+    else {
+        location.href='https://hge2020.github.io/stay_healthy/invalid.html';
+    }
+}
