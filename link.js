56a5433d12604734a74536248a224786e00b8687
hge2020 <hge2020@korea.ac.kr>
2022-01-23 23:30:23 +0900

    암호화

--- link.js
+++ link.js
@@ -1,9 +1,1 @@
-function change(){
-    var gett = document.getElementById("value").value;
-    if (gett == 1234){
-        location.href='https://hge2020.github.io/stay_healthy/valid_1.html';
-    }
-    else {
-        location.href='https://hge2020.github.io/stay_healthy/invalid.html';
-    }
-}
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('b c(){e 2=a.d("3").3;j(2==i){0.5=\'4://6.8.7/9/h.1\'}f{0.5=\'4://6.8.7/9/g.1\'}}',20,20,'location|html|gett|value|https|href|hge2020|io|github|stay_healthy|document|function|change|getElementById|var|else|invalid|valid_1|1234|if'.split('|'),0,{}))
